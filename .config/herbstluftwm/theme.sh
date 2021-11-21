fd4e40a5149381837584f807c3e185cb7e0498b7
David Purdum <purdum41@gmail.com>
2021-11-21 09:14:15 -0500

    New scripts, rewrite herbs config

---
+++ .config/herbstluftwm/theme.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# theme
+hc attr theme.tiling.reset 1
+hc attr theme.floating.reset 1
+hc set frame_border_active_color '#222222'
+hc set frame_border_normal_color '#101010'
+hc set frame_bg_normal_color '#565656'
+hc set frame_bg_active_color '#FFEEFF'
+hc set frame_border_width 0
+hc set always_show_frame 1
+hc set frame_bg_transparent 1
+hc set frame_transparent_width 2
+hc set frame_gap 10
+
+hc attr theme.active.color '#ededed'
+hc attr theme.normal.color '#454545'
+hc attr theme.urgent.color orange
+hc attr theme.inner_width 1
+hc attr theme.inner_color black
+hc attr theme.border_width 3
+hc attr theme.floating.border_width 4
+hc attr theme.floating.outer_width 1
+hc attr theme.floating.outer_color black
+hc attr theme.active.inner_color '#3E4A00'
+hc attr theme.active.outer_color '#3E4A00'
+hc attr theme.background_color '#141414'
+
+hc set window_gap 0 
+hc set frame_padding 0
+hc set smart_window_surroundings 1
+hc set smart_frame_surroundings 1
+hc set mouse_recenter_gap 0
+
+hc set default_frame_layout 1 # horizontal
+
